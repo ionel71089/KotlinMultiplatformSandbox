6fb7c4405133151468ea872018922654b37ca708
ionel71089 <ionel.lescai@flir.com>
2023-04-11 11:30:25 +0300

    Add Http Request

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -1,6 +1,15 @@
+val ktorVersion = "2.2.4"
+
+version = "0.1"
+
 plugins {
     kotlin("multiplatform")
     id("com.android.library")
+    kotlin("plugin.serialization") version "1.8.20"
+    kotlin("native.cocoapods")
+
+//    id("co.touchlab.faktory.kmmbridge") version "0.3.7"
+//    `maven-publish`
 }
 
 kotlin {
@@ -26,33 +35,56 @@ kotlin {
         val commonMain by getting {
             dependencies {
                 implementation("org.jetbrains.kotlinx:kotlinx-datetime:0.4.0")
+                implementation("io.ktor:ktor-client-core:$ktorVersion")
+                implementation("io.ktor:ktor-client-content-negotiation:$ktorVersion")
+                implementation("io.ktor:ktor-serialization-kotlinx-json:$ktorVersion")
             }
         }
+
         val commonTest by getting {
             dependencies {
                 implementation(kotlin("test"))
+                implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.4")
             }
         }
-        val androidMain by getting
+
+        val androidMain by getting {
+            dependencies {
+                implementation("io.ktor:ktor-client-android:$ktorVersion")
+            }
+        }
         val androidUnitTest by getting
+
         val iosX64Main by getting
         val iosArm64Main by getting
         val iosSimulatorArm64Main by getting
+
         val iosMain by creating {
             dependsOn(commonMain)
             iosX64Main.dependsOn(this)
             iosArm64Main.dependsOn(this)
             iosSimulatorArm64Main.dependsOn(this)
+            dependencies {
+                implementation("io.ktor:ktor-client-darwin:$ktorVersion")
+            }
         }
+
         val iosX64Test by getting
         val iosArm64Test by getting
         val iosSimulatorArm64Test by getting
+
         val iosTest by creating {
             dependsOn(commonTest)
             iosX64Test.dependsOn(this)
             iosArm64Test.dependsOn(this)
             iosSimulatorArm64Test.dependsOn(this)
         }
+    }
+
+    cocoapods {
+        summary = "KMMBridgeSampleKotlin"
+        homepage = "https://touchlab.dev"
+        ios.deploymentTarget = "13"
     }
 }
 
@@ -64,3 +96,14 @@ android {
         targetSdk = 33
     }
 }
+
+/*
+addGithubPackagesRepository()
+kmmbridge {
+    mavenPublishArtifacts()
+    githubReleaseVersions()
+    spm()
+    cocoapods("git@github.com:ionel71089/PublicPodspecs.git")
+    versionPrefix.set("0.8")
+}
+ */
